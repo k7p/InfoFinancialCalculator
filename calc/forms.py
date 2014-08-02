7ca55d8839c409fec7e06ef82c41071bad30d4e0
Kishan Panchal <kishanpanchal@mbka.att.net>
2014-08-02 13:51:47 -0700

    First commit

---
+++ calc/forms.py
@@ -0,0 +1,11 @@
+from django import forms
+
+class CalcForm(forms.ModelForm):
+	class Meta:
+		#model = 
+		fields = ['']
+
+
+		'''email = request.POST.get('email')
+username = request.POST.get('username')
+password = request.POST.get('password') '''
